aa5387cb8a75d84e8ecc0913ecc715577093f43a
Juan Sebastian <j.nunezc@uniandes.edu.co>
2024-11-02 14:28:00 -0500

    Update configuration

--- src/config/config.py
+++ src/config/config.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config:
-    db_user = os.getenv("DB_USER", "default_user")
-    db_pass = os.getenv("DB_PASSWORD", "default_pass")
-    db_host = os.getenv("DB_HOST", "localhost")
-    db_port = os.getenv("DB_PORT", "5432")
-    db_name = os.getenv("DB_NAME", "default_db")
+    db_user = "postgres"
+    db_pass = "secret"
+    db_host = "localhost"
+    db_port = "5432"
+    db_name = "blacklist_db"
 
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
